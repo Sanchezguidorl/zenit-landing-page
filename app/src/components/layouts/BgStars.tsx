112d1b7a896564dc0004ab53a44f4ab3bd7c03b3
Guido Abel Sanchez <melaniecaseros1989@proton.me>
2024-06-09 16:14:40 -0300

    Se configura tailwind para que purgue css no utilizado

--- app/src/components/layouts/BgStars.tsx
+++ app/src/components/layouts/BgStars.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import "../styles/layouts/BgStars.css"
 import Twinkling from '/public/twinkling.svg';
+import Stars from '/public/stars.png';
 function BgStars() {
   return (
     <div className="background-container fadeIn">
-    <div className="stars"></div>
+    <Image src={Stars} alt='' quality={5} className="stars"/>
     <Image src={Twinkling} alt="" loading='eager' quality={5} className="twinkling"/>
   </div>
   )
