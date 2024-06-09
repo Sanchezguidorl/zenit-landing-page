a7f0d1f372fd94ba8a2a3e04982dd4cfdb3adf36
Guido Abel Sanchez <melaniecaseros1989@proton.me>
2024-06-09 15:27:46 -0300

    Se modifica calidad de imagen para efecto

--- app/src/components/layouts/BgStars.tsx
+++ app/src/components/layouts/BgStars.tsx
@@ -5,7 +5,7 @@ function BgStars() {
   return (
     <div className="background-container fadeIn">
     <div className="stars"></div>
-    <Image src={Twinkling} alt="" className="twinkling"/>
+    <Image src={Twinkling} alt="" loading='eager' quality={5} className="twinkling"/>
   </div>
   )
 }
