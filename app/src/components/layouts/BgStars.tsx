25e04d2cc983ce5c29626afeda4220dc7fd0f938
Guido Abel Sanchez <melaniecaseros1989@proton.me>
2024-06-09 21:36:03 -0300

    se modifican tamaños de imágenes

--- app/src/components/layouts/BgStars.tsx
+++ app/src/components/layouts/BgStars.tsx
@@ -1,14 +1,28 @@
-import "../../../styles/layouts/BgStars.css"
-import Image from 'next/image'
-import Stars from '/public/stars.png';
-import Twinkling from '/public/twinkling.svg';
+import "../../../styles/layouts/BgStars.css";
+import Image from "next/image";
+import Stars from "/public/stars.png";
+import Twinkling from "/public/twinkling.svg";
 function BgStars() {
   return (
     <div className="background-container fadeIn">
-    <Image src={Stars} alt='' layout="responsive" priority sizes="(max-width: 430px) 400px, 100vw" quality={2} className="stars"/>
-    <Image src={Twinkling} alt="" layout="responsive" priority sizes="(max-width: 430px) 400px, 100vw" quality={2} className="twinkling"/>
-  </div>
-  )
+      <Image
+        src={Stars}
+        alt=""
+        priority
+        sizes="(max-width: 430px) 400px, 100vw"
+        quality={2}
+        className="stars"
+      />
+      <Image
+        src={Twinkling}
+        alt=""
+        priority
+        sizes="(max-width: 430px) 400px, 100vw"
+        quality={2}
+        className="twinkling"
+      />
+    </div>
+  );
 }
 
-export default BgStars
+export default BgStars;
