25094ed350e0a9d00f56d167994546ad4eb6f17c
Guido Abel Sanchez <melaniecaseros1989@proton.me>
2024-06-20 23:05:55 -0300

    Se modifica cursor 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,62 +6,26 @@ import CursorCustom from "./src/components/layouts/CursorCustom";
 
 export const metadata = {
   title: "Páginas web profesionales para marcas de éxito",
-  description:
-    "Desarrollamos Páginas web para marcas de alto valor | SEO y Posicionamiento Web",
+  description: "Desarrollamos Páginas web para marcas de alto valor | SEO y Posicionamiento Web",
   keywords: [
-    "programador",
-    "desarrollador web",
-    "WordPress",
-    "SEO",
-    "posicionamiento",
-    "páginas web",
-    "landing pages",
-    "desarrollo web",
-    "diseño web",
-    "optimización web",
-    "Argentina",
-    "Buenos Aires",
-    "CABA",
-    "WordPress Developer",
-    "Desarrollo en WordPress",
-    "Personalización de Temas WordPress",
-    "Optimización SEO",
-    "Posicionamiento Web",
-    "SEO Local",
-    "Página web",
-    "Página Web",
-    "Sitio Web",
-    "Sitio web",
-    "Backlinks",
-    "Diseño Web Responsivo",
-    "Experiencia de Usuario",
-    "Diseño de Interfaz",
-    "Diseño de Sitios Web",
-    "Desarrollo Web Personalizado",
-    "Programación a Medida",
-    "Soluciones Web a la Medida",
-    "Marketing Online",
-    "Contenido Web",
-    "Estrategias de Contenido",
-    "Redes Sociales",
-    "Landing Pages para Marcas",
-    "Landing Pages de Producto",
-    "Optimización de Conversiones",
-    "Página para negocio",
-    "Community manager",
-    "Community management"
+    "programador", "desarrollador web", "WordPress", "SEO", "posicionamiento",
+    "páginas web", "landing pages", "desarrollo web", "diseño web", "optimización web",
+    "Argentina", "Buenos Aires", "CABA", "WordPress Developer", "Desarrollo en WordPress",
+    "Personalización de Temas WordPress", "Optimización SEO", "Posicionamiento Web",
+    "SEO Local", "Página web", "Página Web", "Sitio Web", "Sitio web", "Backlinks",
+    "Diseño Web Responsivo", "Experiencia de Usuario", "Diseño de Interfaz",
+    "Diseño de Sitios Web", "Desarrollo Web Personalizado", "Programación a Medida",
+    "Soluciones Web a la Medida", "Marketing Online", "Contenido Web", "Estrategias de Contenido",
+    "Redes Sociales", "Landing Pages para Marcas", "Landing Pages de Producto",
+    "Optimización de Conversiones", "Página para negocio", "Community manager", "Community management"
   ],
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body className="relative h-fit">
-        <CursorCustom/>
+        <CursorCustom />
         <BgStars />
         <div className="absolute h-full w-full overflow-hidden">
           <BgEffectContainer />
